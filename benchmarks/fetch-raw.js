fe9af5cc597b4902c3dd2ad9d371a960794b69b8
RafaelGSS <rafael.nunu@hotmail.com>
2022-10-19 14:26:21 -0300

    fix benchmark fetch-raw

--- benchmarks/fetch-raw.js
+++ benchmarks/fetch-raw.js
@@ -1,5 +1,6 @@
 const { fetch, setGlobalDispatcher, Agent } = require('../index')
 const { WritableStream } = require('stream/web')
+const { Writable } = require('stream')
 
 const samples = 100
 const parallelRequests = Array.from(Array(1000))
@@ -9,7 +10,17 @@ setGlobalDispatcher(new Agent({ connections: 50 }))
 function performRequest() {
   return new Promise((resolve) => {
     return fetch('http://localhost:3001').then(res => {
-      res.body.pipeTo(new WritableStream({ write () {}, close () { resolve() } }))
+      // res.body.pipeTo(new WritableStream({ write () {}, close () { resolve() } }))
+      res.body
+        .pipe(
+          new Writable({
+            write (chunk, encoding, callback) {
+              console.log('chunk', chunk.toString())
+              callback()
+            }
+          })
+        )
+        .on('finish', resolve)
     })
   })
 }
